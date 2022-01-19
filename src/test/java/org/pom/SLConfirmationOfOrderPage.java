e68d44a13bb80120e0d9fe7fb21e2648260bc5f5
Michael <michael_makam18@hotmail.com>
2022-01-19 11:56:24 +0000

    created ConfirmationOfOrderPage

---
+++ src/test/java/org/pom/SLConfirmationOfOrderPage.java
@@ -0,0 +1,12 @@
+package org.pom;
+
+import org.openqa.selenium.WebDriver;
+
+public class SLConfirmationOfOrderPage {
+
+    private WebDriver driver;
+
+    public SLConfirmationOfOrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
