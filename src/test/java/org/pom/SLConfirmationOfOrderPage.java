ef9ce6a29d111f02b8caac3069ee72f2574ac779
Michael <michael_makam18@hotmail.com>
2022-01-19 12:16:34 +0000

    updated ConfirmationOfOrderPage with returnToInventoryPage()

--- src/test/java/org/pom/SLConfirmationOfOrderPage.java
+++ src/test/java/org/pom/SLConfirmationOfOrderPage.java
@@ -1,12 +1,21 @@
 package org.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SLConfirmationOfOrderPage {
+public class SLConfirmationOfOrderPage extends SitePage {
 
+    private By backToProduct = new By.ById("back-to-products");
     private WebDriver driver;
 
     public SLConfirmationOfOrderPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    public SLInventoryPage returnToInventoryPage() {
+        driver.findElement(backToProduct).click();
+        return new SLInventoryPage(driver);
+    }
+
+
 }
