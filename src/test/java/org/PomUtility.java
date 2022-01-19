21723fa618f1b7ca2bdc8c4b9d3eadc7b34d2069
Pawel <pawel.dyjak@poczta.onet.pl>
2022-01-19 13:44:18 +0000

    Utility class added

---
+++ src/test/java/org/PomUtility.java
@@ -0,0 +1,23 @@
+package org;
+
+import org.openqa.selenium.chrome.ChromeDriverService;
+import java.io.File;
+
+public class PomUtility {
+    private static String defaultDriverLocation = "src/test/resources/chromedriver.exe";
+
+    public static void setDriverLocation(String pathToDriver){
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+    }
+
+    public static ChromeDriverService setChromeDriverService(String pathToDriver){
+        return new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(pathToDriver))
+                .usingAnyFreePort()
+                .build();
+    }
+
+    public static String getDefaultDriverLocation() {
+        return defaultDriverLocation;
+    }
+}
