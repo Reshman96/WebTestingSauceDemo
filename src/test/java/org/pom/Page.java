d7e44fb0c5664edf9ceeb76bfa40cc5db52eaab5
JamesDorling <jamesrdorling@gmail.com>
2022-01-19 12:03:12 +0000

    Filled out abstract classes with empty methods to fill in with selenium

--- src/test/java/org/pom/Page.java
+++ src/test/java/org/pom/Page.java
@@ -1,4 +1,10 @@
 package org.pom;
 
+import org.openqa.selenium.WebDriver;
+
 public abstract class Page {
+    private WebDriver driver;
+    public Page(WebDriver webDriver) {
+        this.driver = webDriver;
+    }
 }
