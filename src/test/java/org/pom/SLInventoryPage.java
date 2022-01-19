86f7c23566fb7ceb40aa1c87c5edc8c096bac101
JamesDorling <jamesrdorling@gmail.com>
2022-01-19 13:56:27 +0000

    added findelements to the inventory page

--- src/test/java/org/pom/SLInventoryPage.java
+++ src/test/java/org/pom/SLInventoryPage.java
@@ -1,5 +1,6 @@
 package org.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SLInventoryPage extends SitePage{
@@ -11,14 +12,17 @@ public class SLInventoryPage extends SitePage{
     }
 
     public SLIndividualItemPage goToItem(int itemNo) {
+        driver.findElements(By.className("inventory_item")).get(itemNo).findElement(By.className("inventory_item_name")).click();
         return new SLIndividualItemPage(driver);
     }
 
-    public SLCartPage goToCart() {
-        return new SLCartPage(driver);
+    public SLInventoryPage addToCart(int itemNo) {
+        driver.findElements(By.className("inventory_item")).get(itemNo).findElement(By.className("btn_inventory")).click();
+        return this;
     }
 
-    SLInventoryPage addToCart(int itemNo) {
-        return this;
+    public SLCartPage goToCart() {
+        driver.findElement(By.id("shopping_cart_container")).click();
+        return new SLCartPage(driver);
     }
 }
