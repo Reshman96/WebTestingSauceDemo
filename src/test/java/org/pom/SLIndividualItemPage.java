d09d363747b3251a83db1aeba6e71318e6944c7f
Pawel <pawel.dyjak@poczta.onet.pl>
2022-01-19 12:14:24 +0000

    class name changed

--- src/test/java/org/pom/SLIndividualItemPage.java
+++ src/test/java/org/pom/SLIndividualItemPage.java
@@ -3,7 +3,7 @@ package org.pom;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SLSauceLabsBackpackPage extends Page {
+public class SLIndividualItemPage extends Page {
     private WebDriver driver;
     private By addToCartButton = new By.ById("add-to-cart-sauce-labs-backpack");
     private By removeButton = new By.ById("remove-sauce-labs-backpack");
@@ -13,7 +13,7 @@ public class SLSauceLabsBackpackPage extends Page {
     private By productPrice = new By.ByClassName("inventory_details_price");
     private By backToProductsButton = new By.ById("back-to-products");
 
-    public SLSauceLabsBackpackPage(WebDriver driver) {
+    public SLIndividualItemPage(WebDriver driver) {
         this.driver = driver;
     }
 
