e787ed89c5bf51e6e5aeb8b04761f6f19c060c41
Hanif Ali <ali.hanif@outlook.com>
2022-01-20 16:52:34 +0000

    refactor LoginStepdefs

--- src/test/java/org/bddTest/stepDefs/LoginStepdefs.java
+++ src/test/java/org/bddTest/stepDefs/LoginStepdefs.java
@@ -17,58 +17,57 @@ public class LoginStepdefs {
 
     @Given("I am on the login page")
     public void iAmOnTheLoginPage() {
-        //DriversFactory.driverLoader(DriversFactory.BrowsersEnums.CHROME);
         driver = StepDefsUtil.driver;
         loginPage = new SlLoginPage(driver);
     }
 
     @When("I enter valid username and password")
     public void iEnterValidUsernameAndPassword() {
-        inventoryPage = loginPage.Login("standard_user", "secret_sauce");
+        StepdefData.getLoginPage().Login("standard_user", "secret_sauce");
     }
 
     @Then("I will be logged in")
     public void iWillBeLoggedIn() {
-        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", inventoryPage.getURL());
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", StepdefData.getInventoryPage().getURL());
     }
 
     @When("I provide incorrect username and password")
     public void iProvideIncorrectUsernameAndPassword() {
-        inventoryPage = loginPage.Login("incorrect", "incorrect");
+        StepdefData.getLoginPage().Login("incorrect", "incorrect");
     }
 
     @Then("I will not be logged in")
     public void iWillNotBeLoggedIn() {
-        Assertions.assertEquals("https://www.saucedemo.com/", inventoryPage.getURL());
+        Assertions.assertEquals("https://www.saucedemo.com/", StepdefData.getLoginPage().getURL());
     }
 
     @When("I enter valid username and password but the account is locked out")
     public void iEnterValidUsernameAndPasswordButTheAccountIsLockedOut() {
-        inventoryPage = loginPage.Login("locked_out_user", "secret_sauce");
+        StepdefData.getLoginPage().Login("locked_out_user", "secret_sauce");
     }
 
     @When("I leave the username or password box blank")
     public void iLeaveTheUsernameOrPasswordBoxBlank() {
-        inventoryPage = loginPage.Login("", "");
+        StepdefData.getLoginPage().Login("", "");
     }
 
     @When("I enter valid username and password but the account is a problem user")
     public void iEnterValidUsernameAndPasswordButTheAccountIsAProblemUser() {
-        inventoryPage = loginPage.Login("problem_user", "secret_sauce");
+        StepdefData.getLoginPage().Login("problem_user", "secret_sauce");
     }
 
     @When("I enter valid username and password but the account is a performance glitch user")
     public void iEnterValidUsernameAndPasswordButTheAccountIsAPerformanceGlitchUser() {
-        inventoryPage = loginPage.Login("performance_glitch_user", "secret_sauce");
+        StepdefData.getLoginPage().Login("performance_glitch_user", "secret_sauce");
     }
 
     @Then("the product images will be wrong")
     public void theProductImagesWillBeWrong() {
-        Assertions.assertEquals("https://www.saucedemo.com/static/media/sl-404.168b1cce.jpg", inventoryPage.getInventoryElementImageLink(1));
+        Assertions.assertEquals("https://www.saucedemo.com/static/media/sl-404.168b1cce.jpg", StepdefData.getInventoryPage().getInventoryElementImageLink(1));
     }
 
     @Then("the product images will be correct")
     public void theProductImagesWillBeCorrect() {
-        Assertions.assertEquals("https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.34e7aa42.jpg", inventoryPage.getInventoryElementImageLink(0));
+        Assertions.assertEquals("https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.34e7aa42.jpg", StepdefData.getInventoryPage().getInventoryElementImageLink(0));
     }
 }
