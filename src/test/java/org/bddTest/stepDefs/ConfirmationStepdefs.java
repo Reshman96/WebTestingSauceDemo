8d4a50343c501b7b8b3af7fe1de76ad419e9e94b
Michael <michael_makam18@hotmail.com>
2022-01-20 12:16:31 +0000

    created ConfirmationStepdefs.java

---
+++ src/test/java/org/bddTest/stepDefs/ConfirmationStepdefs.java
@@ -0,0 +1,36 @@
+package org.bddTest.stepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.PomUtility;
+import org.openqa.selenium.WebDriver;
+import org.pom.SLConfirmationOfOrderPage;
+import org.pom.SlLoginPage;
+import org.pom.inventoryPage.SLInventoryPage;
+
+public class ConfirmationStepdefs {
+    private WebDriver driver;
+    private SlLoginPage loginPage;
+    private SLInventoryPage inventoryPage;
+    private SLConfirmationOfOrderPage confirmationPage;
+
+    @And("I am on the confirmation page")
+    public void iAmOnTheConfirmationPage() {
+        PomUtility.setDriverLocation(PomUtility.getDefaultDriverLocation());
+        PomUtility.setChromeDriverService(PomUtility.getDefaultDriverLocation());
+        driver = StepDefsUtil.driver;
+        loginPage = new SlLoginPage(driver);
+        inventoryPage = loginPage.Login("standard_user", "secret_sauce");
+        confirmationPage = inventoryPage.goToCart()
+                .goToCheckoutStep1Page()
+                .goToCheckoutStep2Page()
+                .goToConfirmationPage();
+
+
+    }
+
+    @When("I click on back home")
+    public void iClickOnBackHome() {
+        inventoryPage = confirmationPage.returnToInventoryPage();
+    }
+}
